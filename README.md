666fe7c3ecc67f093260af909d2661a1e62759c8
Cleffy <falchard@live.com>
2023-07-16 15:56:23 -0700

    Prompt for initials generated
    Created a prompt to get 3 characters from the user

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SVG Logo Maker
  Creates a Logo in .svg format based on a color and a shape.
+
+
+## Credits
+inquirer
+jest
+https://javascript.plainenglish.io/
---
+++ index.js
@@ -0,0 +1,51 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+/**
+ * 
+ */
+class SVGLogoMaker{
+    constructor(){
+        this.initials;
+    }
+    async buildLogo(){
+        try{
+            await this.getInitials();
+            console.log(this.initials);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+    /**
+     * getInitials()
+     * Prompts the user to enter three characters
+     * Returns three characters
+     */
+    async getInitials(){
+        await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'initials',
+                message: 'Enter up to three characters to place in the center of the logo.',
+            },
+        ]).then((answers) => {
+            if(answers.initials.length < 1){
+                console.log("You didn't enter any characters.");
+                return this.getInitials();
+            }
+            else if(answers.initials.length > 3){
+                console.log("You entered more than 3 characters.");
+                return this.getInitials();
+            }
+            else{
+                this.initials = answers.initials;
+            }
+        });
+    }
+}
+
+let svgLogo = new SVGLogoMaker();
+svgLogo.buildLogo();
+
+export default { };
